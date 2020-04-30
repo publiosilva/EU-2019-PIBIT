6df4ae24d2b138370fa001a80ad1fa0c3e92e26f
Públio Blenílio Tavares Silva <publio.blenilio@gmail.com>
2020-04-30 15:37:57 -0300

    Update notebooks

--- feature_model_cluster_labeling_with_VALE_method/script.py
+++ feature_model_cluster_labeling_with_VALE_method/script.py
@@ -13,26 +13,26 @@ def get_cluster_score(cluster_path):
                 if i > 0:
                     if i in directly_proportional_values_columns:
                         if column == 'veryLow':
-                            cluster_score_counter += 1
+                            cluster_score_counter += 0.1
                         elif column == 'low':
-                            cluster_score_counter += 2
+                            cluster_score_counter += 0.2
                         elif column == 'moderate':
-                            cluster_score_counter += 3
+                            cluster_score_counter += 0.3
                         elif column == 'high':
-                            cluster_score_counter += 4
+                            cluster_score_counter += 0.4
                         elif column == 'veryHigh':
-                            cluster_score_counter += 5
+                            cluster_score_counter += 0.5
                     else:
                         if column == 'veryLow':
-                            cluster_score_counter += 5
+                            cluster_score_counter += 0.5
                         elif column == 'low':
-                            cluster_score_counter += 4
+                            cluster_score_counter += 0.4
                         elif column == 'moderate':
-                            cluster_score_counter += 3
+                            cluster_score_counter += 0.3
                         elif column == 'high':
-                            cluster_score_counter += 2
+                            cluster_score_counter += 0.2
                         elif column == 'veryHigh':
-                            cluster_score_counter += 1
+                            cluster_score_counter += 0.1
 
             number_of_feature_models += 1
 
@@ -40,16 +40,19 @@ def get_cluster_score(cluster_path):
 
 
 if __name__ == '__main__':
-    cluster_score_list = []
-    cluster_label_list = ['very bad', 'bad', 'moderate', 'good', 'very good']
+    base_path = 'k-means/3-clusters/'
+    cluster_label_list = ['bad', 'moderate', 'good']
+    initial_cluster, final_cluster = 0, 2
 
-    for i in range(0, 5):
+    cluster_score_list = []
+
+    for i in range(initial_cluster, final_cluster + 1):
         cluster_score_list.append(
-            {'cluster': 'cluster ' + str(i), 'score': get_cluster_score('cluster-' + str(i) + '.csv')})
+            {'cluster': 'cluster ' + str(i), 'score': get_cluster_score(base_path + 'cluster-' + str(i) + '.csv')})
 
     cluster_score_list = sorted(cluster_score_list, key=lambda i: i['score'])
 
-    with open('result.txt', 'w') as file:
+    with open(base_path + 'result.txt', 'w') as file:
         for i, cluster_score in enumerate(cluster_score_list):
             file.write(cluster_score['cluster'] +
-                       ' : ' + cluster_label_list[i] + '\n')
+                       ' : ' + cluster_label_list[i] + ' (' + str(cluster_score['score']) + ')' + '\n')
--- feature_model_cluster_labeling_with_expert_data/script.py
+++ feature_model_cluster_labeling_with_expert_data/script.py
@@ -4,7 +4,7 @@ def get_cluster_score(cluster_path):
 
     with open(cluster_path) as file:
         for line in file:
-            columns = line.split(';')
+            columns = line.split(',')
 
             for i, column in enumerate(columns):
                 value = 0
@@ -139,16 +139,19 @@ def get_cluster_score(cluster_path):
 
 
 if __name__ == '__main__':
-    cluster_score_list = []
-    cluster_label_list = ['very bad', 'bad', 'moderate', 'good', 'very good']
+    base_path = 'k-means/3-clusters/'
+    cluster_label_list = ['bad', 'moderate', 'good']
+    initial_cluster, final_cluster = 0, 2
 
-    for i in range(0, 5):
+    cluster_score_list = []
+
+    for i in range(initial_cluster, final_cluster + 1):
         cluster_score_list.append(
-            {'cluster': 'cluster ' + str(i), 'score': get_cluster_score('cluster-' + str(i) + '.csv')})
+            {'cluster': 'cluster ' + str(i), 'score': get_cluster_score(base_path + 'cluster-' + str(i) + '.csv')})
 
     cluster_score_list = sorted(cluster_score_list, key=lambda i: i['score'])
 
-    with open('result.txt', 'w') as file:
+    with open(base_path + 'result.txt', 'w') as file:
         for i, cluster_score in enumerate(cluster_score_list):
             file.write(cluster_score['cluster'] +
-                       ' : ' + cluster_label_list[i] + '\n')
+                       ' : ' + cluster_label_list[i] + ' (' + str(cluster_score['score']) + ')' + '\n')
